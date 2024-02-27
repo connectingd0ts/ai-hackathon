6cdcd25db827fd4b3ebcdfc93827a67fd2d69a96
a-khanna <akhanna992@gmail.com>
2024-02-28 00:41:46 +0530

    Add some code changes to check pipeline

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World'
 
+@app.route('/test')
+def test():
+    return 'This is a test response'
+
 if __name__ == '__main__':
     app.run()
